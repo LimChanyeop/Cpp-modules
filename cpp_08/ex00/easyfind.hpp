5699e1317e9504e9302eab915aaad298999fcd0a
chanyeop <153893@naver.com>
2022-04-28 13:28:18 +0900

    cpp08/ex00

---
+++ cpp_08/ex00/easyfind.hpp
@@ -0,0 +1,57 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <exception>
+#include <vector>
+#include <array>
+#include <stack>
+#include <queue>
+
+template <typename T>
+int		easyfind(T con, int occur){
+	typename T::iterator it;
+	it = std::find (con.begin(), con.end(), occur);
+	if (it != con.end())
+		return (it - con.begin());
+	return -1;
+}
+
+template <typename T>
+int	easyfind(std::stack<T> st, int occur) //LIFO
+{
+	size_t tmp = st.size();
+	std::vector<T> v;
+	for (size_t i = 0; i < tmp; i++){
+		v.push_back(st.top());
+		st.pop();
+	}
+	return easyfind(v, occur);
+}
+
+template <typename T>
+int		easyfind(std::queue<T> st, int occur) //FIFO
+{
+	size_t tmp = st.size();
+	std::vector<T> v;
+	for (size_t i = 0; i < tmp; i++){
+		v.push_back(st.front());
+		st.pop();
+	}
+	return easyfind(v, occur);
+}
+
+template <typename T>
+int		easyfind(std::priority_queue<T> st, int occur) //FIFO
+{
+	size_t tmp = st.size();
+	std::vector<T> v;
+	for (size_t i = 0; i < tmp; i++){
+		v.push_back(st.top());
+		st.pop();
+	}
+	return easyfind(v, occur);
+}
+
+#endif
