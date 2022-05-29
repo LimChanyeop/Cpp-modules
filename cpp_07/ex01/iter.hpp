b48ed39cdfac7b63ccf142b17032f853d0c0e0e8
chanyeop <153893@naver.com>
2022-05-29 23:59:53 +0900

    clear useless files

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -1,13 +1,11 @@
 #ifndef ITER_HPP
-# define ITER_HPP
+#define ITER_HPP
 
-# include <iostream>
+#include <iostream>
 
-template <typename T>
-void	iter(T* arr, size_t length, void (*f)(T const& t))
-{
-	for (size_t i = 0; i < length; i++)
-		f(arr[i]);
+template <typename T> void iter(T *arr, size_t length, void (*f)(T const &t)) {
+    for (size_t i = 0; i < length; i++)
+        f(arr[i]);
 }
 
 #endif
--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -1,50 +1,53 @@
 #include "iter.hpp"
 
-class Awesome
-{
-private:
-	int _n;
-public:
-	Awesome( void ) : _n( 42 ) { return; }
-	int get( void ) const { return this->_n; }
+class Awesome {
+  private:
+    int _n;
+
+  public:
+    Awesome(void) : _n(42) { return; }
+    int get(void) const { return this->_n; }
 };
 
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
-
-template< typename T >
-void print( T const & x ) { std::cout << x << std::endl; return; }
-
-template< typename T >
-void print2( T const & x ) { std::cout << x << " "; return; }
-
-template< typename T >
-void	add(T const & t)
-{
-	const_cast<T &>(t)++;
+std::ostream &operator<<(std::ostream &o, Awesome const &rhs) {
+    o << rhs.get();
+    return o;
 }
 
-int main() {
-	int tab[] = { 0, 1, 2, 3, 4 };
-	char tab1[] = "Hello world!";
-	Awesome tab2[5];
+template <typename T> void print(T const &x) {
+    std::cout << x << std::endl;
+    return;
+}
 
-    std::cout << "print digit array" << std::endl; 
-	iter( tab, 5, print );
-	iter( tab, 5, add );
-	iter( tab, 5, add );
+template <typename T> void print2(T const &x) {
+    std::cout << x << " ";
+    return;
+}
+
+template <typename T> void add(T const &t) { const_cast<T &>(t)++; }
+
+int main() {
+    int tab[] = {0, 1, 2, 3, 4};
+    char tab1[] = "Hello world!";
+    Awesome tab2[5];
+
+    std::cout << "print digit array" << std::endl;
+    iter(tab, 5, print);
+    iter(tab, 5, add);
+    iter(tab, 5, add);
     std::cout << "after add * 2, print2" << std::endl;
-	iter( tab, 5, print2 );
-	std::cout << std::endl;
+    iter(tab, 5, print2);
+    std::cout << std::endl;
 
     std::cout << "print2 string" << std::endl;
-	iter( tab1, 12, print2);
-	std::cout << std::endl;
-	iter( tab1, 12, add );
+    iter(tab1, 12, print2);
+    std::cout << std::endl;
+    iter(tab1, 12, add);
     std::cout << "after add, print2" << std::endl;
-	iter( tab1, 12, print2);
-	std::cout << std::endl;
-    std::cout << "print digit array set Default: 42" << std::endl; 
-	iter( tab2, 5, print );
+    iter(tab1, 12, print2);
+    std::cout << std::endl;
+    std::cout << "print digit array set Default: 42" << std::endl;
+    iter(tab2, 5, print);
 
-	return 0;
+    return 0;
 }
